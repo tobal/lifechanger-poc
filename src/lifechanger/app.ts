eb44725fd10acc6952d9487275d28189b0c8675f
Tobal <tobal17@gmail.com>
2015-12-23 16:13:10 +0100

    feat(lifechanger): add lifechanger app
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ src/lifechanger/app.ts
@@ -0,0 +1,17 @@
+
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'lifechanger-app',
+	providers: [],
+	directives: [],
+	pipes: [],
+	styles: [],
+	template: require('./app.html')
+})
+// @RouteConfig([
+// 	{ path: '/', component: Home, name: 'Home' }
+// ])
+export class LifechangerApp {
+	constructor() { }
+}
