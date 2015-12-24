fbabe60e6f54bb7fc6eb9982a4bc2c4913a6aac4
Tobal <tobal17@gmail.com>
2015-12-24 13:45:05 +0100

    feat(charsheet): can add nodes
    Signed-off-by: Tobal <tobal17@gmail.com>

--- src/lifechanger/charsheet/charsheet.ts
+++ src/lifechanger/charsheet/charsheet.ts
@@ -3,16 +3,41 @@ import {Component} from 'angular2/core';
 import {Node} from './node'
 
 @Component({
-  selector: 'charsheet',
-  directives: [],
-  providers: [],
-  pipes: [],
-  styles: [],
-  template: require('./charsheet.html')
+    selector: 'charsheet',
+    directives: [],
+    providers: [],
+    pipes: [],
+    styles: [],
+    template: require('./charsheet.html')
 })
 export class CharSheet {
-  sheet: Array<Node>;
-  constructor() {
-    this.sheet = [];
-  }
+    sheet: Array<Node>;
+    addFormVisible: boolean;
+    title: string;
+    type: string;
+    nodeValue: number;
+    constructor() {
+        this.sheet = [];
+        this.addFormVisible = false;
+    }
+
+    formToggle() {
+        this.addFormVisible = !this.addFormVisible;
+    }
+
+    addNodeToSheet() {
+        let node = new Node();
+        node.title = this.title;
+        node.value = this.nodeValue;
+        this.sheet.push(node);
+
+        this.title = '';
+        this.type = '';
+        this.nodeValue = 0;
+        this.addFormVisible = false;
+    }
+
+    typeIsValue() {
+        return this.type === 'value';
+    }
 }
