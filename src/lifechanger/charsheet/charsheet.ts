1e996c76b160a4c6add4cc6e9d4417018f2bd3a3
Tobal <tobal17@gmail.com>
2015-12-28 18:18:42 +0100

    docs(charsheet): example code for saving to file
    Signed-off-by: Tobal <tobal17@gmail.com>

--- src/lifechanger/charsheet/charsheet.ts
+++ src/lifechanger/charsheet/charsheet.ts
@@ -1,7 +1,9 @@
 import {Component, View} from 'angular2/core';
 
-import {NodeArray} from './node/node'
-import {NodeView} from './node/nodeview'
+import {NodeArray} from './node/node';
+import {NodeView} from './node/nodeview';
+
+// import {readJson, writeJson} from 'typings/dist/utils/fs';
 
 @Component({
     selector: 'charsheet',
@@ -13,4 +15,22 @@ export class CharSheet {
     constructor() {
         this.sheet = new NodeArray('Character');
     }
+
+    // saveSheet() {
+    //     // writeJson('charsheet', this.sheet);
+    //     let fs = require('graceful-fs');
+    //     let jsonString = JSON.stringify(this.sheet);
+    //     fs.writeFile('charsheet', jsonString, function(err) {
+    //         if (err) throw err;
+    //     });
+    // }
+
+    // loadSheet() {
+    //     // this.sheet = readJson('charsheet');
+    //     let fs = require('fs');
+    //     fs.readFile('charsheet', 'utf8', function(err, data) {
+    //         if (err) throw err;
+    //         this.sheet = JSON.parse(data);
+    //     });
+    // }
 }
