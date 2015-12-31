0e63a4cc680773e484a7a2b80aa300522cbbd5a1
Tobal <tobal17@gmail.com>
2015-12-31 15:40:32 +0100

    feat(quest): can add quests
    Signed-off-by: Tobal <tobal17@gmail.com>

--- src/lifechanger/charsheet/quest/addquest.ts
+++ src/lifechanger/charsheet/quest/addquest.ts
@@ -1,6 +1,7 @@
 
 import {Component, View} from 'angular2/core';
 
+import {NodeArray, NodeValue} from '../node/node'
 import {Quest, QuestReward} from './quest';
 
 @Component({
@@ -13,20 +14,46 @@ import {Quest, QuestReward} from './quest';
 })
 export class AddQuest {
     questarray: Array<Quest>;
+    sheet: NodeArray;
     addFormVisible: boolean = false;
+    sheetValues: Array<SheetValue>;
     questTitle: string = '';
     questDescription: string = '';
+    questType: string = '';
     questRewards: Array<QuestReward>;
+    questRewardNode: NodeValue;
+    questRewardValue: number = 0;
 
     constructor() {
         this.questRewards = [];
+        this.sheetValues = [];
     }
 
     showAddQuestForm() {
+        this.updateSheetValues();
         this.addFormVisible = true;
+    }
+
+    updateSheetValues() {
+        this.sheetValues = [new SheetValue(new NodeValue('asd', 3), 'caption')];
     }
 
     addQuest() {
         this.addFormVisible = false;
+        this.questarray.push(new Quest(this.questTitle, this.questDescription, this.questType, this.questRewards));
+        this.questTitle = '';
+        this.questDescription = '';
+        this.questType = '';
+        this.questRewards = [];
     }
+
+    addReward() {
+        this.questRewards.push(new QuestReward(this.questRewardNode, this.questRewardValue));
+        this.questRewardNode = undefined;
+        this.questRewardValue = 0;
+    }
+}
+
+class SheetValue {
+    constructor(public node: NodeValue, public caption: string) {}
 }
--- src/lifechanger/charsheet/quest/quest.ts
+++ src/lifechanger/charsheet/quest/quest.ts
@@ -2,11 +2,12 @@
 import {NodeValue} from '../node/node'
 
 export class Quest {
-
+    constructor(public title: string = '',
+    			public description: string = '',
+				public type: string = '',
+    			public rewards: Array<QuestReward> = []) {}
 }
 
 export class QuestReward {
-	constructor(public node: NodeValue, public rewardValue: number) {
-
-	}
+	constructor(public node: NodeValue, public rewardValue: number) {}
 }
